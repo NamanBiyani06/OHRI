a67807f6513b071fb135a2c10f899b134ab15397
Naman Biyani <99156947+NamanBiyani06@users.noreply.github.com>
2023-07-19 10:09:09 -0400

    Add files via upload

---
+++ Code/custom_output.py
@@ -0,0 +1,12 @@
+# this file is used for printing customized outputs when I don't have the correct data for a report
+import pandas as pd
+
+data = pd.read_excel("CleanData\data.xlsx")
+df = pd.DataFrame(data)
+
+transfers = df['patransf_where'].value_counts()
+print(transfers.sort_index())
+
+freq_percent = df.patransf_where.value_counts(normalize=True).mul(100).round(2).astype(str) + '%'
+print(freq_percent.sort_index())
+
