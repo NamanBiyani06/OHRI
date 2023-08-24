f0c95d7e82345aaa5f5455bc3da401b228551b02
Naman Biyani <99156947+NamanBiyani06@users.noreply.github.com>
2023-08-24 14:44:40 -0400

    Update dataset_creator.py

--- Code/KFREValidation/dataset_creator.py
+++ Code/KFREValidation/dataset_creator.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 from xlsxwriter import *
 from datetime import datetime
+from sklearn.metrics import roc_auc_score
 
 # loading data
 validation_data = pd.read_excel('KFRE/validation.xlsx')
@@ -51,11 +52,14 @@ five_yr_four_var['kfre_2021'] = kfre['kfre_4var_5y_2021']
 two_yr_eight_var['kfre_2021'] = kfre['kfre_8var_2y_2021']
 five_yr_eight_var['kfre_2021'] = kfre['kfre_8var_5y_2021']
 
+# ! two yr four var
 exclude_condition_1 = ((two_yr_four_var['date_difference'] <= 730) & (two_yr_four_var['other'] == 1))
 exclude_condition_2 = (two_yr_four_var['kfre_2009'].isnull() | two_yr_four_var['kfre_2021'].isnull())
+exclude_condition_3 = (two_yr_four_var['date_difference'] < 0)
 
-two_yr_four_var.loc[exclude_condition_1, ['excluded', 'exclusion_premise']] = [True, 'Tracked less than 2 years']
+two_yr_four_var.loc[exclude_condition_1, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Tracked less than 2 years', np.nan, np.nan]
 two_yr_four_var.loc[exclude_condition_2, ['excluded', 'exclusion_premise']] = [True, 'No KFRE Data']
+two_yr_four_var.loc[exclude_condition_3, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Negative Dates', np.nan, np.nan]
 
 outcome_condition_1 = ((two_yr_four_var['date_difference'] < 731) & (two_yr_four_var['trans/dialysis'] == 1) & (two_yr_four_var['excluded'] == False))
 two_yr_four_var.loc[outcome_condition_1, 'outcome'] = 1
@@ -64,9 +68,66 @@ two_yr_four_var.loc[outcome_condition_1, 'outcome'] = 1
 outcome_condition_2 = ((two_yr_four_var['outcome'].isnull()) & (two_yr_four_var['excluded'] == False))
 two_yr_four_var.loc[outcome_condition_2, 'outcome'] = 0
 
+# ! two yr eight var
+exclude_condition_1 = ((two_yr_eight_var['date_difference'] <= 730) & (two_yr_eight_var['other'] == 1))
+exclude_condition_2 = (two_yr_eight_var['kfre_2009'].isnull() | two_yr_eight_var['kfre_2021'].isnull())
+exclude_condition_3 = (two_yr_eight_var['date_difference'] < 0)
+
+two_yr_eight_var.loc[exclude_condition_1, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Tracked less than 2 years', np.nan, np.nan]
+two_yr_eight_var.loc[exclude_condition_2, ['excluded', 'exclusion_premise']] = [True, 'No KFRE Data']
+two_yr_eight_var.loc[exclude_condition_3, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Negative Dates', np.nan, np.nan]
+
+outcome_condition_1 = ((two_yr_eight_var['date_difference'] < 730) & (two_yr_eight_var['trans/dialysis'] == 1) & (two_yr_eight_var['excluded'] == False))
+two_yr_eight_var.loc[outcome_condition_1, 'outcome'] = 1
+
+# FIXME: The Issue with this code is that I am allocating a 0 to everything that does not have a 1. This allows for logic holes.
+outcome_condition_2 = ((two_yr_eight_var['outcome'].isnull()) & (two_yr_eight_var['excluded'] == False))
+two_yr_eight_var.loc[outcome_condition_2, 'outcome'] = 0
+
+# ! five yr four var
+exclude_condition_1 = ((five_yr_four_var['date_difference'] <= 1825) & (five_yr_four_var['other'] == 1))
+exclude_condition_2 = (five_yr_four_var['kfre_2009'].isnull() | five_yr_four_var['kfre_2021'].isnull())
+exclude_condition_3 = (five_yr_four_var['date_difference'] < 0)
+
+five_yr_four_var.loc[exclude_condition_1, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Tracked less than 5 years', np.nan, np.nan]
+five_yr_four_var.loc[exclude_condition_2, ['excluded', 'exclusion_premise']] = [True, 'No KFRE Data']
+five_yr_four_var.loc[exclude_condition_3, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Negative Dates', np.nan, np.nan]
+
+outcome_condition_1 = ((five_yr_four_var['date_difference'] < 1825) & (five_yr_four_var['trans/dialysis'] == 1) & (five_yr_four_var['excluded'] == False))
+five_yr_four_var.loc[outcome_condition_1, 'outcome'] = 1
+
+# FIXME: The Issue with this code is that I am allocating a 0 to everything that does not have a 1. This allows for logic holes.
+outcome_condition_2 = ((five_yr_four_var['outcome'].isnull()) & (five_yr_four_var['excluded'] == False))
+five_yr_four_var.loc[outcome_condition_2, 'outcome'] = 0
+
+# ! five yr eight var
+exclude_condition_1 = ((five_yr_eight_var['date_difference'] <= 1825) & (five_yr_eight_var['other'] == 1))
+exclude_condition_2 = (five_yr_eight_var['kfre_2009'].isnull() | five_yr_eight_var['kfre_2021'].isnull())
+exclude_condition_3 = (five_yr_eight_var['date_difference'] < 0)
+
+five_yr_eight_var.loc[exclude_condition_1, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Tracked less than 5 years', np.nan, np.nan]
+five_yr_eight_var.loc[exclude_condition_2, ['excluded', 'exclusion_premise']] = [True, 'No KFRE Data']
+five_yr_eight_var.loc[exclude_condition_3, ['excluded', 'exclusion_premise', 'kfre_2009', 'kfre_2021']] = [True, 'Negative Dates', np.nan, np.nan]
+
+outcome_condition_1 = ((five_yr_eight_var['date_difference'] < 1825) & (five_yr_eight_var['trans/dialysis'] == 1) & (five_yr_eight_var['excluded'] == False))
+five_yr_eight_var.loc[outcome_condition_1, 'outcome'] = 1
+
+# FIXME: The Issue with this code is that I am allocating a 0 to everything that does not have a 1. This allows for logic holes.
+outcome_condition_2 = ((five_yr_eight_var['outcome'].isnull()) & (five_yr_eight_var['excluded'] == False))
+five_yr_eight_var.loc[outcome_condition_2, 'outcome'] = 0
+
 
 # print(two_yr_four_var.head(15))
 
+# two_yr_four_var = two_yr_four_var.dropna(subset=['outcome', 'kfre_2021'])
+
+# c_statistic = roc_auc_score(two_yr_four_var['outcome'], two_yr_four_var['kfre_2021'])
+# print(c_statistic)
+
+# print(two_yr_four_var['kfre_2021'].corr(two_yr_four_var['outcome']))
+
 # export to validation folder
 two_yr_four_var.to_excel('Validation/two_yr_four_var.xlsx', index=False ,engine='xlsxwriter')
-
+two_yr_eight_var.to_excel('Validation/two_yr_eight_var.xlsx', index=False ,engine='xlsxwriter')
+five_yr_four_var.to_excel('Validation/five_yr_four_var.xlsx', index=False ,engine='xlsxwriter')
+five_yr_eight_var.to_excel('Validation/five_yr_eight_var.xlsx', index=False ,engine='xlsxwriter')
